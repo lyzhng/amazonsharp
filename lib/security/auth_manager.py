c50a74ec0c8fd9fc7fc3899c483cc19e007d46a9
Patrick Chan <patrickchan1999@gmail.com>
2019-05-02 20:22:17 -0400

    Help differentiate errors from success using 'Error: ' prefix

--- lib/security/auth_manager.py
+++ lib/security/auth_manager.py
@@ -45,7 +45,7 @@ def is_developer(username: str) -> bool:
 def register(username: str, password: str, role: str) -> Tuple[bool, str]:
     "Add the user to the database."
     if is_registered(username):
-        return False, 'Username already exists!'
+        return False, 'Error: Username already exists!'
     hashed_password: str = security_utils.secure_hash_password(password)
     __AUTH_MANAGER.insert('login_info', username, hashed_password, role)
     return True, 'Successfully registered!'
@@ -54,17 +54,17 @@ def register(username: str, password: str, role: str) -> Tuple[bool, str]:
 def login(username: str, password: str) -> Tuple[bool, str]:
     "Check if the credentials are correct."
     if not is_registered(username):
-        return False, 'The username and/or password is invalid.'
+        return False, 'Error: The username and/or password is invalid.'
 
     user_info_cond: str = ' email = "{}" '.format(username)
     user_info = __AUTH_MANAGER.retrieve_rows('login_info', '*', user_info_cond)
 
     if passlib.hash.argon2.verify(password, user_info[0][1]):
         return True, 'Successfully logged in!'
-    return False, 'The username and/or password is invalid.'
+    return False, 'Error: The username and/or password is invalid.'
 
 
 def drop_user(username: str):
     if not is_registered(username):
-        return False, 'User does not exists!'
+        return False, 'Error: User does not exists!'
     __AUTH_MANAGER.delete('login_info', ' email = "{}" ')
