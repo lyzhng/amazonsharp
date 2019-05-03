bde0e2821cdbe1af48a73e852e0ee4ffddae7b3a
Patrick Chan <patrickchan1999@gmail.com>
2019-05-02 20:21:23 -0400

    Update app with new location of static files

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from lib.security import security
 
 # FREE FLOATING SECTION STARTS HERE
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_folder='web/public/components/',
+    static_url_path='')
 app.register_blueprint(public.PUBLIC_VIEWS)
 app.register_blueprint(security.SECURITY)
 
