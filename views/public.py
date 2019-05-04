bd8a72a50118389c4a2a39f29c5d90d27836e8e8
Lily Zhong <lilyzhongy@gmail.com>
2019-05-03 20:21:56 -0400

    display items

--- views/public.py
+++ views/public.py
@@ -10,9 +10,14 @@ import flask
 from lib.security import security
 
 PUBLIC_VIEWS = flask.Blueprint('public_views', __name__)
+__DATABASE = db_manager.DatabaseManager(config.get_value(config.DB_NAME))                                                            
 
 
 @PUBLIC_VIEWS.route('/')
 @PUBLIC_VIEWS.route('/home')
 def home():
     return flask.render_template('home.html', is_logged_in=security.is_logged_in())
+
+@PUBLIC_VIEWS.route('/items')
+def items():
+    return flask.render_template('items.html', is_logged_in=security.is_logged_in(), items=__DATABASE.retrieve_all_items())
