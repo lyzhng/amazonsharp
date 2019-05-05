8134494c4c9f2f88c3acb37ee0e53ac4ad44b283
Lily Zhong <lilyzhongy@gmail.com>
2019-05-05 14:48:07 -0400

    add db_manager without lock and update script file

---
+++ lib/db_manager/db_manager_no_lock.py
@@ -0,0 +1,262 @@
+import sqlite3
+from typing import List
+
+
+class DatabaseManager:
+
+
+    def __init__(self, filename: str):  
+        self.conn = sqlite3.connect(filename, check_same_thread=False) 
+        self.conn.execute('PRAGMA foreign_keys = 1')
+        self.cur = self.conn.cursor()
+
+    def create_table(self, create_table_statement: str) -> None:
+        self.cur.execute(create_table_statement)
+        self.conn.commit()
+
+    def insert(self, table_name: str, *values) -> None:
+        if table_name.upper() == 'ITEM':
+            self.insert_item(*values)
+            self.insert_if_item(table_name, *values)
+            return
+        qmark = ("?," * len(values)).rstrip(",")
+        with self.conn:
+            self.insert_if_user(table_name, *values)
+            self.cur.execute('INSERT INTO {} VALUES({})'.format(table_name, qmark), values)
+            self.insert_if(table_name, *values) 
+
+    def insert_if(self, table_name: str, *values) -> None:
+        self.insert_if_items_bought(table_name, *values)
+        self.insert_if_customer(table_name, *values)
+
+    def insert_if_user(self, table_name: str, *values) -> None:
+        is_customer = (table_name.upper() == 'CUSTOMER')
+        is_seller = (table_name.upper() == 'SELLER')
+        is_employee = (table_name.upper() == 'EMPLOYEE')
+        if is_customer or is_seller:
+            email = values[0]
+            values = ('first_name', 'last_name', email)
+            self.insert('USER', *values)
+
+    def insert_if_customer(self, table_name: str, *values) -> None:
+        if table_name != 'CUSTOMER':
+            return
+        if self.count_rows('CUSTOMER', '*') == 1:
+            self.insert('SHOPPING_CART', 1, 0, 0)
+            self.insert('HAS_SHOPPING_CART', values[0], 1)
+        else:
+            current_max = self.retrieve_max_cart_id()[0]
+            self.insert('SHOPPING_CART', current_max + 1, 0, 0)
+            self.insert('HAS_SHOPPING_CART', values[0], current_max + 1)   
+
+    def retrieve_max_cart_id(self) -> int:
+        self.cur.execute(
+            """
+            SELECT cart_id
+            FROM has_shopping_cart
+            ORDER BY cart_id DESC
+            LIMIT 1
+            """
+        )
+        return self.cur.fetchone()
+
+    def insert_if_items_bought(self, table_name: str, *values) -> None:
+        if table_name.upper() == 'ITEMS_BOUGHT':
+            seller_email, item_id, order_number, price, name, item_type, number_of_items_bought = values
+            self.cur.execute(
+                """
+                INSERT INTO item_frequency(seller_email, item_id, frequency)
+                VALUES('{}', {}, {})
+                ON CONFLICT(seller_email, item_id) 
+                DO UPDATE
+                SET frequency = frequency + {}
+                """
+                .format(
+                    seller_email, item_id, number_of_items_bought,
+                    number_of_items_bought
+                )
+            )
+            self.conn.commit()
+            self.cur.execute(
+                """
+                UPDATE item
+                SET quantity = quantity - {}
+                WHERE seller_email = (SELECT seller_email FROM items_bought WHERE item.seller_email = items_bought.seller_email AND item.item_id = items_bought.item_id AND items_bought.order_number = {})
+                    AND item_id = (SELECT item_id FROM items_bought WHERE item.seller_email = items_bought.seller_email AND item.item_id = items_bought.item_id AND items_bought.order_number = {})
+                """
+                .format(
+                    order_number, order_number,
+                    number_of_items_bought
+                )
+            )
+            self.conn.commit()
+
+    def insert_item(self, *values) -> None:
+        seller_email, item_id, quantity, price, name, item_type = values
+        self.cur.execute(
+            """
+            INSERT OR REPLACE INTO item(seller_email, item_id, quantity, price, name, type)
+            VALUES('{}', {}, {}, {}, '{}', '{}')
+            """
+            .format(seller_email, item_id, quantity, price, name, item_type)
+        )
+        self.conn.commit()
+
+    def insert_if_item(self, table_name: str, *values):
+        if table_name.upper() == 'ITEM':
+            seller_email, item_id, quantity, price, name, item_type = values
+            self.cur.execute(
+                """
+                INSERT INTO inventory(seller_email, item_id)
+                VALUES('{}', {})
+                ON CONFLICT(seller_email, item_id)
+                DO NOTHING
+                """
+                .format(seller_email, item_id)
+            )
+            self.conn.commit()
+
+
+    def update(self, table_name: str, modifications: str, filters: str) -> None:
+        self.cur.execute('UPDATE {} SET {} WHERE {}'.format(table_name, modifications, filters))
+        self.conn.commit()
+
+    def delete(self, table_name: str, filters: str) -> None:
+        self.cur.execute('DELETE {} WHERE {}'.format(table_name, filters))
+        self.conn.commit()
+
+    def delete_user(self, filters: str) -> None:
+        self.delete('USER', '{}'.format(filters))
+        self.conn.commit()
+
+    def has_rows(self, table_name: str, selected_attributes: str, filters: str = None) -> bool:
+        return self.count_rows(table_name, selected_attributes, filters) > 0
+
+    def count_rows(self, table_name: str, selected_attributes: str, filters: str = None) -> int:
+        return len(self.retrieve_rows(table_name, selected_attributes, filters))
+
+    def retrieve_rows(self, table_name: str, selected_attributes: str, filters: str = None) -> List:
+        if filters is None:
+            self.cur.execute('SELECT {} FROM {}'.format(selected_attributes, table_name))
+        else:
+            self.cur.execute('SELECT {} FROM {} WHERE {}'.format(selected_attributes, table_name, filters))
+        try:
+            return self.cur.fetchall()  
+        except sqlite3.OperationalError:
+            return []
+
+    def retrieve_row(self, table_name: str, selected_attributes: str, filters: str = None) -> List:
+        if filters is None:
+            self.cur.execute('SELECT {} FROM {}'.format(selected_attributes, table_name))
+        else:
+            self.cur.execute('SELECT {} FROM {} WHERE {}'.format(selected_attributes, table_name, filters))
+        try:
+            return self.cur.fetchone()  
+        except sqlite3.OperationalError:
+            return []
+
+
+    # transactions with database #
+
+    def add_to_cart(self, customer_email: str, item_id: int) -> None:
+        pass
+
+    def retrieve_popular_items(self, row_count: int = None):
+        if row_count is None:
+            row_count = self.count_rows('ITEMS_BOUGHT', '*')
+        self.cur.execute(
+            """
+            SELECT items_bought.name, items_bought.price, item_frequency.frequency
+            FROM item_frequency INNER JOIN items_bought
+                ON item_frequency.seller_email = items_bought.seller_email 
+                AND item_frequency.item_id = items_bought.item_id
+            GROUP BY item_frequency.seller_email, item_frequency.item_id
+            ORDER BY frequency DESC
+            LIMIT {}
+            """
+            .format(row_count)
+        )
+        try:
+            popular_items = [(entry[0], '$' + str(entry[1]), entry[2]) for entry in self.cur.fetchall()]
+            return popular_items
+        except sqlite3.OperationalError:
+            return []
+
+    def retrieve_all_items(self):
+        try:
+            all_items = [(entry[0], '$' + str(entry[1])) for entry in self.retrieve_rows('ITEM', 'name, price')]
+            return all_items
+        except sqlite3.OperationalError:
+            return []
+
+    def retrieve_available_items(self):
+        try:
+            return self.retrieve_rows('ITEM', 'item_id, name, quantity, price', ' quantity > 0 ')
+        except sqlite3.OperationalError:
+            return []
+
+    def retrieve_out_of_stock_items(self):
+        try:
+            return self.retrieve_rows('ITEM', 'item_id, name, quantity, price', ' quantity = 0')
+        except sqlite3.OperationalError:
+            return []
+
+    def retrieve_sellers(self):
+        try:
+            return self.retrieve_rows('SELLER', 'email, address, phone_number')
+        except sqlite3.OperationalError:
+            return []
+
+    def retrieve_items_by_seller(self, seller_email: str) -> List:
+        self.cur.execute(
+            """
+            SELECT item.seller_email, item.item_id, item.name, item.price, item.quantity
+            FROM item
+            WHERE item.seller_email = '{}'
+            """
+            .format(seller_email)
+        )
+        items_by_seller = [(entry[0], entry[1], entry[2], '$' + str(entry[3]), entry[4]) for entry in self.cur.fetchall()]
+        return items_by_seller
+
+    def retrieve_orders_by_customer(self, customer_email: str) -> List:
+        try:
+            return self.retrieve_rows('ORDER_PLACED', 'customer_email', " customer_email = '{}' ".format(customer_email))
+        except sqlite3.OperationalError:
+            return []
+
+    def retrieve_items_from_order(self, order_number: int) -> List:
+        try:
+            return self.retrieve_rows('ITEMS_BOUGHT', 'name, number_of_items_bought', ' order_number = {} '.format(order_number))
+        except sqlite3.OperationalError:
+            return []            
+        
+    # testing
+    def retrieve_items_from_shopping_cart(self, customer_email: str) -> List:
+        self.cur.execute(
+            """ 
+            SELECT C.item_id, C.name, C.number_of_items_bought
+            FROM has_a_shopping_cart A
+                LEFT OUTER JOIN shopping_cart B
+                    ON A.cart_id == B.cart_id
+                INNER JOIN items_in_shopping_cart C
+                    ON A.cart_id == C.cart_id
+            WHERE A.email == "{}"
+            """
+            .format(cusomer_email)
+        )
+        return self.cur.fetchall()
+
+    def delete_table(self, table_name: str) -> None:
+        self.cur.execute('DROP TABLE {}'.format(table_name))
+        self.cur.commit()
+
+    def close_db(self) -> None:
+        self.cur.close()
+        self.conn.close()
+
+    def get_cursor(self):
+        return self.cur
+
+    def get_conn(self):
+        return self.conn
--- scripts/add_items_to_db.py
+++ scripts/add_items_to_db.py
@@ -12,10 +12,10 @@ import names
 import random
 import secrets
 from lib.config import config
-from lib.db_manager import db_manager
+from lib.db_manager import db_manager_no_lock
 from lib.db_manager import db_create_constants as CREATE_CONSTANTS
 
-manager = db_manager.DatabaseManager(config.get_value(config.DB_NAME))
+manager = db_manager_no_lock.DatabaseManager(config.get_value(config.DB_NAME))
 trailing_address = ['Street', 'Road', 'City']
 
 # tables
@@ -25,13 +25,13 @@ manager.create_table(CREATE_CONSTANTS.CUSTOMER) # works
 manager.create_table(CREATE_CONSTANTS.EMPLOYEE) # works
 
 manager.create_table(CREATE_CONSTANTS.ITEM) # works
-manager.create_table(CREATE_CONSTANTS.INVENTORY)
+manager.create_table(CREATE_CONSTANTS.INVENTORY) 
+manager.create_table(CREATE_CONSTANTS.SHOPPING_CART) # works
 manager.create_table(CREATE_CONSTANTS.HAS_SHOPPING_CART) # works
 manager.create_table(CREATE_CONSTANTS.ORDERS) # works
-manager.create_table(CREATE_CONSTANTS.ORDER_PLACED) #
-manager.create_table(CREATE_CONSTANTS.ITEMS_BOUGHT)
-manager.create_table(CREATE_CONSTANTS.SHOPPING_CART)
-manager.create_table(CREATE_CONSTANTS.ITEM_FREQUENCY) #
+manager.create_table(CREATE_CONSTANTS.ORDER_PLACED) 
+manager.create_table(CREATE_CONSTANTS.ITEMS_BOUGHT) 
+manager.create_table(CREATE_CONSTANTS.ITEM_FREQUENCY)
 manager.create_table(CREATE_CONSTANTS.ITEMS_IN_SHOPPING_CART)
 
 customers = []
@@ -63,25 +63,27 @@ for i in range(5):
     manager.insert('SELLER', email, address, phone_number)
     sellers.append(email)
 
-for i in range(5):
-    first_name = names.get_first_name()
-    last_name = names.get_last_name()
-    email = first_name + '.' + last_name + '@email.com'
-    role = random.choice(employee_roles)
-    date_joined = str(random.randint(2000, 2050)) + '-' + str(random.randint(1, 12)) + '-' + str(random.randint(1, 30))
-    phone_number = random.randint(1e9, 9e9)
-    manager.insert('EMPLOYEE', email, role, date_joined, phone_number)
-    employees.append(email)
+# for i in range(5):
+#     first_name = names.get_first_name()
+#     last_name = names.get_last_name()
+#     email = first_name + '.' + last_name + '@email.com'
+#     role = random.choice(employee_roles)
+#     date_joined = str(random.randint(2000, 2050)) + '-' + str(random.randint(1, 12)) + '-' + str(random.randint(1, 30))
+#     phone_number = random.randint(1e9, 9e9)
+#     manager.insert('EMPLOYEE', email, role, date_joined, phone_number)
+#     employees.append(email)
 
+item_sellers = []
 # put 5 items in db
-for i in range(10):
+for i in range(5):
     seller = random.choice(sellers)
-    item_id = random.randint(1, 100)
+    item_id = random.randint(1, 10)
     price = random.randint(1, 10)
     name = 'ITEM: ' + 'name' + str(i)
     item_type = 'ITEM_TYPE: ' + 'type' + str(i)
-    manager.insert('ITEM', seller, item_id, 5, price, name, item_type)
+    manager.insert('ITEM', seller, item_id, 100, price, name, item_type)
     items.append(item_id)
+    item_sellers.append(seller)
 
 for i in range(5):
     order_no = random.randint(1, 1e5)
@@ -90,12 +92,12 @@ for i in range(5):
     manager.insert('ORDERS', order_no, total_number_of_items, date_ordered)
     orders.append(order_no)
 
-items_bought = {}
-for order in orders:
-    item = 12345
-    seller = random.choice(sellers)
+for i in range(len(orders)):
+    item = items[i]
+    item_seller = item_sellers[i]
+    order = orders[i]
     price = random.randint(1, 50)
     name = 'item_name: ' + str(item) + str(random.randint(1, 100))
     item_type = 'item_type: ' + str(item) + str(random.randint(1, 100))
     number_of_items_bought = 3
-    manager.insert('ITEMS_BOUGHT', seller, item, order, price, name, item_type, number_of_items_bought)
+    manager.insert('ITEMS_BOUGHT', item_seller, item, order, price, name, item_type, number_of_items_bought)
