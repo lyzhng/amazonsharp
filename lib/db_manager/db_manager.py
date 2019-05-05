aa3a35069a44c2123fb1a6b10d325ecc855a59b1
Patrick Chan <patrickchan1999@gmail.com>
2019-05-04 21:39:53 -0400

    LOCK everything to avoid SQLITE3 recursive cursor error

--- lib/db_manager/db_manager.py
+++ lib/db_manager/db_manager.py
@@ -1,18 +1,24 @@
 import sqlite3
+import threading
 from typing import List
 
+
+LOCK = threading.Lock();
+
+
 class DatabaseManager:
+
 
     def __init__(self, filename: str):  
         self.conn = sqlite3.connect(filename, check_same_thread=False) 
         self.cur = self.conn.cursor()
 
     def create_table(self, create_table_statement: str) -> None:
-        with self.conn:
+        with self.conn, LOCK:
             self.cur.execute(create_table_statement)
 
     def create_trigger(self, create_trigger_statement: str) -> None:
-        with self.conn:
+        with self.conn, LOCK:
             self.cur.execute(create_trigger_statement)
 
     # general case
@@ -24,7 +30,7 @@ class DatabaseManager:
             self.insert_item(*values)
             return
         qmark = ("?," * len(values)).rstrip(",")
-        with self.conn:
+        with self.conn, LOCK:
             self.cur.execute('INSERT INTO {} VALUES({})'.format(table_name, qmark), values)
             self.insert_if(table_name, *values) 
 
@@ -70,55 +76,61 @@ class DatabaseManager:
     def insert_if_items_bought(self, table_name: str, *values) -> None:
         if table_name.upper() == 'ITEMS_BOUGHT':
             seller_email, item_id, order, price, name, item_type, number_of_items_bought = values
-            self.cur.execute(
-                """
-                INSERT INTO item_frequency(seller_email, item_id, frequency)
-                VALUES('{}', {}, {})
-                ON CONFLICT(seller_email, item_id) 
-                DO UPDATE
-                SET frequency = frequency + {}
-                """
-                .format(seller_email, item_id, number_of_items_bought, number_of_items_bought)
-            )
-            self.conn.commit()
+            with LOCK:
+                self.cur.execute(
+                    """
+                    INSERT INTO item_frequency(seller_email, item_id, frequency)
+                    VALUES('{}', {}, {})
+                    ON CONFLICT(seller_email, item_id) 
+                    DO UPDATE
+                    SET frequency = frequency + {}
+                    """
+                    .format(seller_email, item_id, number_of_items_bought, number_of_items_bought)
+                )
+                self.conn.commit()
 
     def insert_item(self, *values) -> None:
         seller_email, item_id, quantity, price, name, item_type = values
-        self.cur.execute(
-            """
-            INSERT OR REPLACE INTO item(seller_email, item_id, quantity, price, name, type)
-            VALUES('{}', {}, {}, {}, '{}', '{}')
-            """
-            .format(seller_email, item_id, quantity, price, name, item_type)
-        )
-        self.conn.commit()
+        with LOCK:
+            self.cur.execute(
+                """
+                INSERT OR REPLACE INTO item(seller_email, item_id, quantity, price, name, type)
+                VALUES('{}', {}, {}, {}, '{}', '{}')
+                """
+                .format(seller_email, item_id, quantity, price, name, item_type)
+            )
+            self.conn.commit()
 
     def insert_if_item(self, table_name: str, *values):
         if table_name.upper() == 'ITEM':
             seller_email, item_id, quantity, price, name, item_type = values
-            self.cur.execute(
-                """
-                INSERT INTO inventory(seller_email, item_id)
-                VALUES('{}', {})
-                ON CONFLICT(seller_email, item_id)
-                DO NOTHING
-                """
-                .format(seller_email, item_id)
-            )
-            self.conn.commit()
+            with LOCK:
+                self.cur.execute(
+                    """
+                    INSERT INTO inventory(seller_email, item_id)
+                    VALUES('{}', {})
+                    ON CONFLICT(seller_email, item_id)
+                    DO NOTHING
+                    """
+                    .format(seller_email, item_id)
+                )
+                self.conn.commit()
 
 
     def update(self, table_name: str, modifications: str, filters: str) -> None:
-        self.cur.execute('UPDATE {} SET {} WHERE {}'.format(table_name, modifications, filters))
-        self.conn.commit()
+        with LOCK:
+            self.cur.execute('UPDATE {} SET {} WHERE {}'.format(table_name, modifications, filters))
+            self.conn.commit()
 
     def delete(self, table_name: str, filters: str) -> None:
-        self.cur.execute('DELETE {} WHERE {}'.format(table_name, filters))
-        self.conn.commit()
+        with LOCK:
+            self.cur.execute('DELETE {} WHERE {}'.format(table_name, filters))
+            self.conn.commit()
 
     def delete_user(self, filters: str) -> None:
-        self.delete('USER', '{}'.format(filters))
-        self.conn.commit()
+        with LOCK:
+            self.delete('USER', '{}'.format(filters))
+            self.conn.commit()
 
     def has_rows(self, table_name: str, selected_attributes: str, filters: str = None) -> bool:
         return self.count_rows(table_name, selected_attributes, filters) > 0
@@ -127,18 +139,20 @@ class DatabaseManager:
         return len(self.retrieve_rows(table_name, selected_attributes, filters))
 
     def retrieve_rows(self, table_name: str, selected_attributes: str, filters: str = None) -> List:
-        if filters is None:
-            self.cur.execute('SELECT {} FROM {}'.format(selected_attributes, table_name))
-        else:
-            self.cur.execute('SELECT {} FROM {} WHERE {}'.format(selected_attributes, table_name, filters))
-        return self.cur.fetchall()  
+        with LOCK:
+            if filters is None:
+                self.cur.execute('SELECT {} FROM {}'.format(selected_attributes, table_name))
+            else:
+                self.cur.execute('SELECT {} FROM {} WHERE {}'.format(selected_attributes, table_name, filters))
+            return self.cur.fetchall()  
 
     def retrieve_row(self, table_name: str, selected_attributes: str, filters: str = None) -> List:
-        if filters is None:
-            self.cur.execute('SELECT {} FROM {}'.format(selected_attributes, table_name))
-        else:
-            self.cur.execute('SELECT {} FROM {} WHERE {}'.format(selected_attributes, table_name, filters))
-        return self.cur.fetchone()  
+        with LOCK:
+            if filters is None:
+                self.cur.execute('SELECT {} FROM {}'.format(selected_attributes, table_name))
+            else:
+                self.cur.execute('SELECT {} FROM {} WHERE {}'.format(selected_attributes, table_name, filters))
+            return self.cur.fetchone()  
 
     # transactions with database #
 
@@ -148,20 +162,22 @@ class DatabaseManager:
     def retrieve_popular_items(self, row_count: int = None):
         if row_count is None:
             row_count = self.count_rows('ITEMS_BOUGHT', '*')
-        self.cur.execute(
-            """
-            SELECT items_bought.name, items_bought.price, item_frequency.frequency
-            FROM item_frequency INNER JOIN items_bought
-                ON item_frequency.seller_email = items_bought.seller_email 
-                    AND item_frequency.item_id = items_bought.item_id
-            GROUP BY item_frequency.seller_email, item_frequency.item_id
-            ORDER BY frequency DESC
-            LIMIT {}
-            """
-            .format(row_count)
-        )
+        with LOCK:
+            self.cur.execute(
+                """
+                SELECT items_bought.name, items_bought.price, item_frequency.frequency
+                FROM item_frequency INNER JOIN items_bought
+                    ON item_frequency.seller_email = items_bought.seller_email 
+                        AND item_frequency.item_id = items_bought.item_id
+                GROUP BY item_frequency.seller_email, item_frequency.item_id
+                ORDER BY frequency DESC
+                LIMIT {}
+                """
+                .format(row_count)
+            )
         try:
-            popular_items = [(entry[0], '$' + str(entry[1]), entry[2]) for entry in self.cur.fetchall()]
+            with LOCK:
+                popular_items = [(entry[0], '$' + str(entry[1]), entry[2]) for entry in self.cur.fetchall()]
             return popular_items
         except sqlite3.OperationalError:
             return []
@@ -192,19 +208,17 @@ class DatabaseManager:
             return []
 
     def retrieve_items_by_seller(self, seller_email: str) -> List:
-        self.cur.execute(
-            """
-            SELECT item.seller_email, item.item_id, item.name, item.price, item.quantity
-            FROM item
-            WHERE item.seller_email = '{}'
-            """
-            .format(seller_email)
-        )
-        try:
+        with LOCK:
+            self.cur.execute(
+                """
+                SELECT item.seller_email, item.item_id, item.name, item.price, item.quantity
+                FROM item
+                WHERE item.seller_email = '{}'
+                """
+                .format(seller_email)
+            )
             items_by_seller = [(entry[0], entry[1], entry[2], '$' + str(entry[3]), entry[4]) for entry in self.cur.fetchall()]
-            return items_by_seller
-        except sqlite3.OperationalError:
-            return []
+        return items_by_seller
 
     def retrieve_orders_by_customer(self, customer_email: str) -> List:
         try:
@@ -220,27 +234,30 @@ class DatabaseManager:
         
     # testing
     def retrieve_items_from_shopping_cart(self, customer_email: str) -> List:
-        self.cur.execute(
-            """ 
-            SELECT C.item_id, C.name, C.number_of_items_bought
-            FROM has_shopping_cart A
-                LEFT OUTER JOIN shopping_cart B
-                    ON A.cart_id == B.cart_id
-                INNER JOIN items_in_shopping_cart C
-                    ON A.cart_id == C.cart_id
-            WHERE A.email == "{}"
-            """
-            .format(cusomer_email)
-        )
-        return self.cur.fetchall()
+        with LOCK:
+            self.cur.execute(
+                """ 
+                SELECT C.item_id, C.name, C.number_of_items_bought
+                FROM has_a_shopping_cart A
+                    LEFT OUTER JOIN shopping_cart B
+                        ON A.cart_id == B.cart_id
+                    INNER JOIN items_in_shopping_cart C
+                        ON A.cart_id == C.cart_id
+                WHERE A.email == "{}"
+                """
+                .format(cusomer_email)
+            )
+            return self.cur.fetchall()
 
     def delete_table(self, table_name: str) -> None:
-        self.cur.execute('DROP TABLE {}'.format(table_name))
-        self.cur.commit()
+        with LOCK:
+            self.cur.execute('DROP TABLE {}'.format(table_name))
+            self.cur.commit()
 
     def close_db(self) -> None:
-        self.cur.close()
-        self.conn.close()
+        with LOCK:
+            self.cur.close()
+            self.conn.close()
 
     def get_cursor(self):
         return self.cur
