209f3bca6df8e0c8198e4144d2de0d030f0d7555
Patrick Chan <patrickchan1999@gmail.com>
2019-05-05 11:21:14 -0400

    Get image uploading to work

--- views/seller.py
+++ views/seller.py
@@ -7,6 +7,7 @@ if __ROOT_PATH not in sys.path:
     sys.path.append(__ROOT_PATH)
 
 import flask
+import http;
 from lib.security import security
 
 SELLER_VIEWS = flask.Blueprint('seller_views', __name__)
@@ -17,3 +18,14 @@ SELLER_VIEWS = flask.Blueprint('seller_views', __name__)
 def cart():
     return flask.render_template('sell_items.html',
                                  is_logged_in=security.is_logged_in(seller_required=True))
+
+
+@SELLER_VIEWS.route('/upload_image/<seller_email>/<int:item_id>', methods=['POST'])
+@security.login_required(seller_required=True)
+def upload_image(seller_email, item_id):
+    filename: str = flask.request.files.get('image').filename
+    extension: str = filename[filename.rfind('.'):]
+    partial_path: str = f'amazonsharp/web/public/item_images/{seller_email}_{item_id}{extension}' 
+    with open(os.path.join(__ROOT_PATH, partial_path), 'wb') as file_handler:
+        file_handler.write(flask.request.files.get('image').read())
+    return '', http.client.NO_CONTENT
--- web/components/SellItems/NewItem.vue
+++ web/components/SellItems/NewItem.vue
@@ -14,25 +14,92 @@
                 <v-layout row>
                     <v-text-field type="number" label="Quantity" :value="this.quantity" required></v-text-field>
                 </v-layout>
+				<v-layout row>
+					<upload-btn class="ma-0 pa-0" title="Upload Image" accept="image/*" :fileChangedCallback="fileUpload"></upload-btn>
+				</v-layout>
             </v-layout>
         </v-container>
     </v-card-text>
     <v-card-action>
         <v-layout row justify-end>
-            <v-btn color="blue darken-1" flat @click="$emit('input', false)">Save</v-btn>
-            <v-btn color="blue darken-1" flat @click="$emit('input', false)">Quit</v-btn> 
+            <v-btn class="mb-2" color="blue darken-1" flat @click="$emit('input', false)">Save</v-btn>
+            <v-btn class="mb-2" color="blue darken-1" flat @click="$emit('input', false)">Quit</v-btn> 
         </v-layout>
     </v-card-action>
+	<v-dialog v-model="this.fileTypeNotMatch" persistent light max-width="600px">
+		<v-card>
+			<v-card-title>
+				<span class="display-1">Bad File Type</span>
+			</v-card-title>
+			<v-card-text>
+				<span>The uploaded file must be an image type.</span>
+			</v-card-text>
+			<v-card-action>
+				<v-layout row justify-end>
+					<v-btn class="mb-2" color="info" flat @click="removeFileTypeNotMatchDialog">Ok</v-btn>
+				</v-layout>
+			</v-card-action>
+		</v-card>
+	</v-dialog>
+	<v-dialog v-model="this.fileSizeTooBig" persistent light max-width="600px">
+		<v-card>
+			<v-card-title>
+				<span class="display-1">Bad File Size</span>
+			</v-card-title>
+			<v-card-text>
+				<span>The uploaded file must be less than 200KB.</span>
+			</v-card-text>
+			<v-card-action>
+				<v-layout row justify-end>
+					<v-btn class="mb-2" color="info" flat @click="removeFileSizeTooBigDialog">Ok</v-btn>
+				</v-layout>
+			</v-card-action>
+		</v-card>
+	</v-dialog>
 </v-card>
 </template>
 
 <script>
+import UploadButton from 'vuetify-upload-button';
+
 export default {
     name: 'NewItemComponent',
-    components: {},
+    components: {'upload-btn': UploadButton},
     data: () => ({
+		fileUploaded: false,
+		fileTypeNotMatch: false,
+		fileSizeTooBig: false,
     }),
     methods: {
+		removeFileTypeNotMatchDialog() {
+			this.fileTypeNotMatch = false;
+		},
+		removeFileSizeTooBigDialog() {
+			this.fileSizeTooBig = false;
+		},
+		fileUpload(file) {
+			if (!file.type.match('image.*')) {
+				// display type error
+				this.fileTypeNotMatch = true;
+				console.log(this.fileTypeNotMatch);
+				return;
+			} else if (file.size > 200 * 1024) {
+				// display size error
+				this.fileSizeTooBig = true;
+				return;
+			}
+
+			var formData = new FormData();
+			formData.append('image', file);
+
+			// var uploadURL = `/upload_image/${this.seller_email}/${item_id}`;
+			var uploadURL = '/upload_image/testing/123';
+
+			var xhr = new XMLHttpRequest();
+			xhr.open("POST", uploadURL, true);
+			xhr.send(formData);
+			this.fileUploaded = true;
+		}
     },
     props: ['action', 'name', 'price', 'imagePath', 'quantity', 'dialog'],
 }
